5336953df7215a29bca4332fbe307788df016f96
SakuraNotStupid <sakurakyuo@protonmail.com>
2023-07-29 01:55:11 +0800

    Revert "Add and install permission file for Android 9+"
    This reverts commit 6ad1457332bea435e835c2bef0a4426943b3a474.

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -523,15 +523,6 @@ LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/system/bin
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
-#TWRP App permissions for Android 9+
-include $(CLEAR_VARS)
-LOCAL_MODULE := privapp-permissions-twrpapp.xml
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/system/bin
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
 ifeq ($(TW_INCLUDE_CRYPTO), true)
     ifneq ($(TW_CRYPTO_USE_SYSTEM_VOLD),)
         # Prebuilt vdc_pie for pre-Pie SDK Platforms
